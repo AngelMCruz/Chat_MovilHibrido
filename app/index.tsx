e05cccd0a022e0b0763dc4c721417c42326e825f
AngelMCruz <aemolinacruz@gmail.com>
2025-05-27 18:46:00 -0600

    Entrega de repositorio

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { Link, Stack } from "expo-router";
+import { Button, Text, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+
+export default () => {
+    const [username, setUsername] = useState<string>("");
+    return(
+        <SafeAreaView style ={style.container}>
+        <Stack.Screen options={{headerShown : false}}/>
+        <Text>Bienvenido al chat</Text>
+        <TextInput placeholder="Usuario" value={username} onChangeText={t=> setUsername(t)}/>
+        <Link href={{pathname:"/chat", params:{user:username}}} asChild>
+        <Button title="Entrar"/></Link>
+        <Link href= "/settings">Configuración</Link>
+        </SafeAreaView>
+    )
+
+};
+    const style = StyleSheet.create({
+        container:{
+            flex:1,
+            justifyContent:"center",
+            alignItems :"center",
+            gap:8
+        },
+    });
---
+++ src/socket.ts
@@ -0,0 +1,12 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { io } from "socket.io-client";
+
+export async function connectSocket(user: string) {
+    try{
+        const serverAddress = 
+        await AsyncStorage.getItem("serverAddress") || "http://localhost:3000";
+    return io(serverAddress,{query:{user}});
+    } catch (error) {
+        console.error(error);
+    }
+}
